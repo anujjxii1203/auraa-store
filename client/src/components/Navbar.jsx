955c1cc3361942304bedf208adc31059c19f7e41
Anuj rai <anuj2001insta@gmail.com>
2026-07-25 18:34:33 +0530

    fix: change navbar logo link target to Home page (/)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
 
           {/* Logo Section */}
           <div className="logo-left">
-            <Link to="/men" style={{ fontSize: '28px', fontWeight: '900', color: '#e11b23', letterSpacing: '-1.5px', textDecoration: 'none' }}>
+            <Link to="/" style={{ fontSize: '28px', fontWeight: '900', color: '#e11b23', letterSpacing: '-1.5px', textDecoration: 'none' }}>
               AURA STORE
             </Link>
           </div>
